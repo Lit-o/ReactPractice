d2c186bb18b3ea049dcb0ba5ace51aa4998317c4
LitvinenkoBD <boris12lit@gmail.com>
2022-01-21 21:42:12 +0300

    added some basic things for React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
+// import React from 'react'; деструктуризируем и вытаскиваем только Component а не весь React
+import {Component} from 'react';
 import './App.css';
 
+export const Header = () => {
+    return <h2>Gello Gold!</h2>
+}
+
+const Field = () => {
+    const holder = "Enter HERE";
+    const styleField = {
+        width: '300px'
+    }
+    return <input
+        placeholder={holder}
+        type='text'
+        style={styleField} />
+}
+
+const Button = () => {
+    const text = "YES! YES! SAY IT!"
+    const res = () => {
+        return 'Ooo ho ho, i\'m from function!'
+    }
+    const textTag = <p>Yeee, click it</p>
+    const isFromFunc = true
+    return <button>{isFromFunc ? res() : textTag}</button>
+}
+
+class FieldWithClass extends Component {
+    render() {
+        const holder = "Enter Before  Pls";
+        const styleField = {
+            width: '300px'
+        };
+        
+        return <input
+        placeholder={holder}
+        type='text'
+        style={styleField} />
+    }
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header />
+            <Field />
+            <Button />
+            <FieldWithClass/>
+        </div>
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,37 @@
-import React from 'react';
+import React, {StrictMode} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App, { Header } from './App';
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <StrictMode>
+        <App />
+    </StrictMode>,
+    document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
+
+// const title = 'bar'
+// const time = new Date()
+
+// const elem = (
+//   <div>
+//     <h2 >Foo:{title}</h2>
+//     <input type="text" id="input" />
+//     <label htmlFor='input'> Label I quess </label>
+//     <button className="click" tabIndex="0">Oh My!</button>
+//   </div>
+// )
+
+// const elem = <h2>Hello!</h2>
+// const elem = React.createElement('h2', {className: 'red'}, 'Hello World!')
+// const elem = {
+//   type: 'h2',
+//   props: {
+//     className: 'red',
+//     children: 'Hello World'
+//   }
+// }
